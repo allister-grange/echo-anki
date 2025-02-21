079ca70d756daa530642a24b38171c7308f8ae03
Allister Grange <allistergrange@gmail.com>
2025-02-21 20:52:32 +1300

    Chaining together the generate sentence with the text to speech

--- index.js
+++ index.js
@@ -7,15 +7,27 @@ const path = require("path");
 const API_URL = "https://api.openai.com/v1/chat/completions";
 const API_KEY = process.env.OPENAI_API_KEY;
 
-// TODO read in settings from a config file
-
-async function chatWithGPT(prompt) {
+/**
+ *
+ * @param {string[]} knownWords - list words that the user knows to build sentences from
+ * @param {string} targetWord - target word to learn
+ * @param {string} prompt - prompt to create the sentence to be
+ *
+ * @returns {string} sentence with the target word, to then be converted to audio
+ */
+async function generateSentenceFromKnownWords(knownWords, targetWord, prompt) {
   try {
+    const promptWithTargetWordAndLanguage = prompt
+      .replace("<target-word>", targetWord)
+      .replace("<language>", process.env.TARGET_LANGUAGE);
+
+    console.log(promptWithTargetWordAndLanguage);
+
     const response = await axios.post(
       API_URL,
       {
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: prompt }],
+        model: "gpt-4o-mini",
+        messages: [{ role: "user", content: promptWithTargetWordAndLanguage }],
         max_tokens: 5,
       },
       {
@@ -26,7 +38,9 @@ async function chatWithGPT(prompt) {
       }
     );
 
-    console.log("ChatGPT Response:", response.data.choices[0].message.content);
+    console.log("ChatGPT Response:", response.data);
+
+    return response.data.choices[0].message.content;
   } catch (error) {
     console.error(
       "Error:",
@@ -49,8 +63,10 @@ async function textToSpeech(sentence) {
       fs.mkdirSync(destDir);
     }
 
-    const voices = ["nova", "alloy", "echo", "fable", "onyx", "shimmer"];
+    const voices = ["nova", "alloy", "echo", "fable", "shimmer"];
     const randomVoice = voices[Math.floor(Math.random() * voices.length)];
+
+    console.log("Using voice", randomVoice, "to generate", sentence);
 
     const response = await openai.audio.speech.create({
       model: "tts-1",
@@ -64,11 +80,25 @@ async function textToSpeech(sentence) {
     const filePath = path.join(destDir, fileName);
     await fs.promises.writeFile(filePath, buffer);
 
-    console.log(`Audio file saved at: ${filePath}`);
+    console.log(`Audio file saved`);
   } catch (error) {
     console.error("Error generating speech audio:", error.message);
   }
 }
-// Example usage
-// chatWithGPT("Test call for echo-anki.");
-textToSpeech("Bonjour le monde");
+
+async function run() {
+  const example1 = await generateSentenceFromKnownWords(
+    [],
+    "monde",
+    process.env.BEGINNER_PROMPT
+  );
+  const example2 = await generateSentenceFromKnownWords(
+    [],
+    "monde",
+    process.env.ADVANCED_PROMPT
+  );
+  await textToSpeech(example1);
+  await textToSpeech(example2);
+}
+
+run();
